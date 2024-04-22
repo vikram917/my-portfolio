7d38de2ceec582fbb4ea74316239f3f823b93540
vikram917 <baghelvikram917@gmail.com>
2024-04-22 16:05:45 +0530

    Your branch is up to date with 'origin/master'.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,53 +7,34 @@ import Link from 'next/link';
 
 const Sidebar = () => {
   const router = useRouter();
-  const [activeLink, setActiveLink] = useState(router.asPath);
-
+  const [activeLink, setActiveLink] = useState('');
 
   const navigationItems = [
-    {
-      name: 'Home',
-      icon: faHome,
-      href: '/home'
-    },
-    {
-      name: 'About',
-      icon: faUserCircle,
-      href: '/about'
-    },
-    {
-      name: 'Experience',
-      icon: faBriefcase,
-      href: '/experience'
-    },
-    {
-      name: 'Projects',
-      icon: faProjectDiagram,
-      href: '/projects'
-    },
-    {
-      name: 'Contact',
-      icon: faEnvelope,
-      href: '/contact'
-    }
+    { name: 'Home', icon: faHome, href: '/home' },
+    { name: 'About', icon: faUserCircle, href: '/about' },
+    { name: 'Experience', icon: faBriefcase, href: '/experience' },
+    { name: 'Projects', icon: faProjectDiagram, href: '/projects' },
+    { name: 'Contact', icon: faEnvelope, href: '/contact' }
   ];
+
   useEffect(() => {
-    setActiveLink(router.asPath);
+    const active = navigationItems.find(item => router.asPath === item.href);
+    setActiveLink(active ? active.href : 'bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500');
   }, [router.asPath]);
+
   return (
     <div className="bg-transparent text-white w-7 xl:w-14 flex justify-center items-center xl:p-4 rounded-lg">
       <div className="flex flex-col space-y-6">
-      {navigationItems.map((item, index) => (
-      
+        {navigationItems.map((item, index) => (
           <Link
             key={index}
             href={item.href}
-            className={`flex items-center hover:text-white rounded-md xl:p-4 ${
-              router.pathname === item.href ? '' : ''
-            }`}
+            className={`flex items-center hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:text-white rounded-md xl:p-4`}
           >
-            <FontAwesomeIcon icon={item.icon} className={`${router.asPath === item.href ? '' : ''}`}/>
-            <h1></h1>
+            <FontAwesomeIcon
+              icon={item.icon}
+              
+            />
           </Link>
         ))}
       </div>
--- src/utils/data.js
+++ src/utils/data.js
@@ -6,16 +6,7 @@ export const projects = [
       technologies: ['Next', 'React', 'Tailwind'],
       liveUrl: 'https://aeonaxyassignment.netlify.app/',
       githubUrl: 'https://github.com/vikram917/aeonaxy',
-    },
-    {
-      title: 'aeonaxy',
-      description: 'This is a example of resposive pages',
-      imageUrl: '/aeonaxy.png',
-      technologies: ['Next', 'React', 'Tailwind'],
-      liveUrl: 'https://aeonaxyassignment.netlify.app/',
-      githubUrl: 'https://github.com/vikram917/aeonaxy',
-    },
-    
+    }, 
     // Add more projects here
   ];
 
