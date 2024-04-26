787348febc5f5b2cbf5a98ed272b3120e9e0bdba
Vikram Baghel <baghelvikram917@gmail.com>
2024-04-26 16:43:12 +0530

    On branch master

--- src/utils/data.js
+++ src/utils/data.js
@@ -7,6 +7,22 @@ export const projects = [
       liveUrl: 'https://aeonaxyassignment.netlify.app/',
       githubUrl: 'https://github.com/vikram917/aeonaxy',
     }, 
+    {
+      title: 'Portfolio',
+      description: 'Responsive portfolio',
+      imageUrl: '/portfolioimg.png',
+      technologies: ['Next', 'React', 'Tailwind'],
+      liveUrl: 'https://master--vikram917-portfolio.netlify.app/',
+      githubUrl: 'https://github.com/vikram917/my-portfolio',
+    }, 
+    {
+      title: 'Weather-App',
+      description: 'Weather Forcasting app still in development',
+      imageUrl: '/aeonaxy.png',
+      technologies: ['Next', 'Express', 'Tailwind'],
+      liveUrl: '',
+      githubUrl: 'https://github.com/vikram917/weather-app',
+    }, 
     // Add more projects here
   ];
 
