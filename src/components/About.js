667806d69a6d52798828820c3ce824396777146c
Vikram Baghel <baghelvikram917@gmail.com>
2024-04-29 12:10:44 +0530

    On branch master

--- src/components/About.js
+++ src/components/About.js
@@ -57,7 +57,9 @@ const About = () => {
       </div>
       <div className=" w-full md:w-3/5 md:h-full text-slate-300">
         <div>
-          <h3 className="text-lg md:text-4xl font-bold mb-2 mt-2 md:mb-4">Qualifications</h3>
+          <h3 className="text-lg md:text-4xl font-bold mb-2 mt-2 md:mb-4">
+            Qualifications
+          </h3>
           <hr />
           <div className="md:space-y-8 md:mt-4">
             <div className="flex flex-col items-center md:flex-row md:flex md:items-center space-y-2 md:space-y-0 md:space-x-4">
@@ -73,21 +75,19 @@ const About = () => {
                   <p>Govt. Nagarjuna P.G. College, Raipur</p>
                 </div>
               </div>
-            
+
               <p className="w-20 text-center text-xs md:font-semibold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-full md:p-2">
                 Completed
               </p>
-              
-              
             </div>
             <div className="flex flex-col items-center md:flex-row space-y-2 md:space-y-0 md:space-x-4">
               <div>
                 <div className="flex space-x-6 items-center">
                   <h4 className="md:text-xl font-bold">
-                  Bacholer in Computer Application
+                    Bacholer in Computer Application
                   </h4>
                   <p>Percentage - 71.80%</p>
-                <p>2019-2021</p>
+                  <p>2019-2021</p>
                 </div>
                 <div>
                   <p>Govt. Nagarjuna P.G. College, Raipur</p>
@@ -100,11 +100,11 @@ const About = () => {
             <div className="flex flex-col items-center md:flex-row space-y-2 md:space-y-0 md:space-x-4">
               <div>
                 <div className="flex space-x-6 items-center">
-                <h4 className="md:text-xl font-bold">
-                  Diploma in Computer Application
-                </h4>
-                <p>Percentage - 69.25%</p>
-                <p>2018</p>
+                  <h4 className="md:text-xl font-bold">
+                    Diploma in Computer Application
+                  </h4>
+                  <p>Percentage - 69.25%</p>
+                  <p>2018</p>
                 </div>
                 <div>
                   <p>Govt. Nagarjuna P.G. College, Raipur</p>
@@ -117,68 +117,91 @@ const About = () => {
           </div>
         </div>
         <div className="md:space-y-6">
-          <h1 className="text-lg md:text-4xl font-bold mb-2 mt-2 md:mb-4 md:mt-8">Skill's</h1>
+          <h1 className="text-lg md:text-4xl font-bold mb-2 mt-2 md:mb-4 md:mt-8">
+            Skill's
+          </h1>
           <hr />
-          <div className="flex flex-col">
-            <div className="flex justify-evenly flex-wrap items-center ">
-              <img
-                src="/mongodb.png"
-                alt=""
-                className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
-              />
-              <img
-                src="/express.png"
-                alt=""
-                className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out "
-              />
-              <img
-                src="/ract.png"
-                alt=""
-                className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
-              />
-              <img
-                src="/reactnative.png"
-                alt=""
-                className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
-              />
-            </div>
-            <div className="flex flex-wrap justify-evenly items-center ">
-              <img
-                src="/html.png"
-                alt=""
-                className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
-              />
-              <img
-                src="/css.png"
-                alt=""
-                className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
-              />
-              <img
-                src="/js.png"
-                alt=""
-                className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
-              />
-              <img
-                src="/ts.png"
-                alt=""
-                className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
-              />
-              <img
-                src="/mysql.png"
-                alt=""
-                className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
-              />
-              <img
-                src="/firebase.png"
-                alt=""
-                className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
-              />
-              <img
-                src="/python.png"
-                alt=""
-                className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
-              />
-            </div>
+          <div className="flex flex-wrap space-x-4">
+              <div className="xl:w-60">
+                <img
+                  src="/mongodb.png"
+                  alt=""
+                  className="transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
+                />
+              </div>
+               <div className="xl:w-60">
+                <img
+                  src="/express.png"
+                  alt=""
+                  className="transform hover:translate-x-[-5px] transition duration-300 ease-in-out "
+                />
+              </div>
+{/* <div>
+                <img
+                  src="/ract.png"
+                  alt=""
+                  className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
+                />
+              </div>
+
+              <div>
+                <img
+                  src="/reactnative.png"
+                  alt=""
+                  className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
+                />
+              </div>
+            
+              <div>
+                <img
+                  src="/html.png"
+                  alt=""
+                  className="md:w-16 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
+                />
+              </div>
+              <div>
+                <img
+                  src="/css.png"
+                  alt=""
+                  className="md:w-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
+                />
+              </div>
+              <div>
+                <img
+                  src="/js.png"
+                  alt=""
+                  className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
+                />
+              </div>
+              <div>
+                <img
+                  src="/ts.png"
+                  alt=""
+                  className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
+                />
+              </div>
+              <div>
+                <img
+                  src="/mysql.png"
+                  alt=""
+                  className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
+                />
+              </div>
+              <div>
+                <img
+                  src="/firebase.png"
+                  alt=""
+                  className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
+                />
+              </div>
+              <div>
+                <img
+                  src="/python.png"
+                  alt=""
+                  className="h-10 md:h-20 transform hover:translate-x-[-5px] transition duration-300 ease-in-out"
+                />
+              </div> */}
+          
           </div>
         </div>
       </div>
