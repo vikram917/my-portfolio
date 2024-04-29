87aad8ca1f964c59bc586896ebf6eb08a3cef667
Vikram Baghel <baghelvikram917@gmail.com>
2024-04-29 12:27:09 +0530

    Changes to be committed bg:

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,6 @@ import { Lora } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import Footer from "@/components/Footer";
-import Image from "next/image";
-import bg from "../../public/bg.png"
 const nutino = Lora({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${nutino.className} flex flex-col h-screen w-screen text-slate-300 relative`}>
+      <body className={`${nutino.className} flex flex-col h-screen w-screen text-slate-300 bg-black`}>
     
         <div className="p-2 w-full xl:p-10 h-full">
           <div className="w-full h-full backdrop-blur-sm flex flex-row justify-evenly items-center bg-slate-50/2 border border-slate-600  shadow-lg rounded-lg ">
